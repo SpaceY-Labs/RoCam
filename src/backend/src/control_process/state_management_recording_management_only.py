74188a5c355005fb3bd42b600a52471153864c1e
Xiaotian Lou <loux8@mcmaster.ca>
2026-03-10 09:43:34 -0400

    update

--- src/backend/src/control_process/state_management_recording_management_only.py
+++ src/backend/src/control_process/state_management_recording_management_only.py
@@ -11,9 +11,13 @@ logger = logging.getLogger(__name__)
 
 
 class StateManagementRecordingManagementOnly:
+    _FOCAL_MIN_MM = 24.0
+    _FOCAL_MAX_MM = 120.0
+
     def __init__(self):
         self.database = RecordingDatabase(base_path=RECORDING_DATABASE_BASE_PATH)
         self._system_status = SystemStatusMonitor()
+        self._current_focal_length_mm = self._FOCAL_MIN_MM
 
     def arm(self):
         pass
@@ -44,13 +48,14 @@ class StateManagementRecordingManagementOnly:
             is_recording=False,
             longitude=None,
             latitude=None,
-            focal_length_mm=24.0,
-            focal_length_min_mm=24.0,
-            focal_length_max_mm=120.0,
+            focal_length_mm=self._current_focal_length_mm,
+            focal_length_min_mm=self._FOCAL_MIN_MM,
+            focal_length_max_mm=self._FOCAL_MAX_MM,
         )
 
     def set_focal_length(self, focal_length_mm: float):
-        pass
+        clamped = max(self._FOCAL_MIN_MM, min(self._FOCAL_MAX_MM, focal_length_mm))
+        self._current_focal_length_mm = clamped
 
     def manual_move(self, direction: str):
         pass
